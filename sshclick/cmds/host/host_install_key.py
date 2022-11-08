f39eb76a46bcc080ea7b7c620729c9bcfbb4acac
Karlo Tisaj <karlo.tisaj@snt.hr>
2022-11-08 12:09:14 +0100

    Testing installing key on host matching filtering

---
+++ sshclick/cmds/host/host_install_key.py
@@ -0,0 +1,42 @@
+import click
+import subprocess
+from sshclick.sshc import SSH_Config, complete_ssh_host_names, expand_names
+
+#------------------------------------------------------------------------------
+# COMMAND: host test
+#------------------------------------------------------------------------------
+SHORT_HELP = "Install SSH key to hosts (experimental)"
+LONG_HELP  = """
+Installs SSH key to target hosts
+
+NOTE: This command is experimental and might change or be removed in future
+"""
+
+# Parameters help:
+TIME_HELP  = "Timeout for SSH connection"
+#------------------------------------------------------------------------------
+
+@click.command(name="install", short_help=SHORT_HELP, help=LONG_HELP)
+@click.option("--timeout", default=3, help=TIME_HELP)
+@click.option("--password", required=True, help=TIME_HELP)
+@click.argument("name", required=True, shell_complete=complete_ssh_host_names)
+@click.pass_context
+def cmd(ctx, name, timeout, password):
+    config: SSH_Config = ctx.obj
+
+    filtered_groups = config.filter_config(None, name)
+    for group in filtered_groups:
+        for host in group.hosts:
+            hostname = host.name
+
+            #// SSH Command method via subprocess
+            #------------------------------------------------
+            ssh_command = f"sshpass -p {password} ssh-copy-id -f -o StrictHostKeyChecking=no -o ConnectTimeout={timeout} {hostname}"
+            response = subprocess.run(ssh_command, shell=True)
+            
+            print(f"Copy key to: {name} ({hostname}) ", end="")
+            if response.returncode == 0:
+                print("successful!")
+            else:
+                print("failed!")
+    ctx.exit()
